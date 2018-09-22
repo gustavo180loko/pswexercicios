83fc142a58c6f334c843e3ef1279f08d86a94e6d
guga_ <guga_@LAPTOP-QT3SBHFJ>
2018-09-22 19:42:37 -0300

    Heranca entre a SuperClasse Quadrilatero e as subclasses

---
+++ Exercicio2.3/src/Exercicios2/Quadrilatero.java
@@ -0,0 +1,50 @@
+package Exercicios2;
+
+public class Quadrilatero {
+	
+	private double x,y;
+	private double z,w;
+	
+	public Quadrilatero(){
+	}
+	
+	public Quadrilatero(double x, double y, double z, double w) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.w = w;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
+	public double getW() {
+		return w;
+	}
+
+	public void setW(double w) {
+		this.w = w;
+	}	
+	
+}
---
+++ Exercicio2.3/src/Exercicios2/Trapezio.java
@@ -0,0 +1,8 @@
+package Exercicios2;
+
+public class Trapezio extends Quadrilatero{
+	
+	public Trapezio(double x , double y) {
+		super(x,y, y, y);
+	}
+}
